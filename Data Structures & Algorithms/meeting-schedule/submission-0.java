840c01b02c4d7963beb45857522a2f30716dde55
Aniket Gawade <anigawade05@gmail.com>
2026-09-20 11:23:35 +0530

    Add: meeting-schedule - submission-0

---
+++ Data Structures & Algorithms/meeting-schedule/submission-0.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean canAttendMeetings(List<Interval> intervals) {
+        intervals.sort((a, b) -> Integer.compare(a.start, b.start));
+
+        for (int i = 1; i < intervals.size(); i++) {
+            if (intervals.get(i - 1).end > intervals.get(i).start) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
